842656e2464b6aa53f75a7e67d069ef929eb30ff
user <dev@antalogic.com>
2020-08-07 11:50:15 +0300

    Updates

---
+++ precache-manifest.4d7dc2970b4c8791d895817558e043c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9645c9fe9e5333210f8b908eb2c31d",
+    "url": "/themeor-test/index.html"
+  },
+  {
+    "revision": "c90f4cc631276c6c83e4",
+    "url": "/themeor-test/static/css/2.0fec355f.chunk.css"
+  },
+  {
+    "revision": "28c949be8fb6efffa787",
+    "url": "/themeor-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c90f4cc631276c6c83e4",
+    "url": "/themeor-test/static/js/2.6821af7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/themeor-test/static/js/2.6821af7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c949be8fb6efffa787",
+    "url": "/themeor-test/static/js/main.6c7ba8eb.chunk.js"
+  },
+  {
+    "revision": "b52803de5c99851dfcb0",
+    "url": "/themeor-test/static/js/runtime-main.f4510d9f.js"
+  }
+]);
